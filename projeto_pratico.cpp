97d81fdfb5c3b7b350af484d8d5b7b7fbbfcaa72
via <liviarezende04812@gmail.com>
2025-11-25 12:29:41 -0300

    feat: Aloca dinamicamente vetor

--- projeto_pratico.cpp
+++ projeto_pratico.cpp
@@ -15,62 +15,92 @@
 using namespace std;
 
 struct infoSatelite {
-	int identificador;
-	string nome;
-	string paisOrigem;
-	int anoLancamento;
-	string funcao;
+    unsigned int identificador;
+    string nome;
+    string paisOrigem;
+    int anoLancamento;
+    string funcao;
 };
 
-void lerString(string arquivo_csv) {
-	
-	
-}
+void RedimensionaVetor(infoSatelite *&vetor, int &tamanhoVetor);
 
 int main(){
-	//variaveis auxiliares
-	string linha = "";
-	char lixo;
-	int numRegistros = 0;
-	
-	//leitura do arquivo csv
-	ifstream arquivo_csv("db_satelites.csv");
-	
-	if (not(arquivo_csv)) {
-		cout << "Erro: não foi possível localizar o arquivo :(" << endl;
-	}
-	
-	else {
-		getline(arquivo_csv, linha); //leitura da linha de campos
-		
-		arquivo_csv >> numRegistros;
-		arquivo_csv.ignore();
-		
-		infoSatelite *satelites = new infoSatelite[numRegistros];
-		
-		for (int i = 0; i < numRegistros; i++) {
-			
-			arquivo_csv >> satelites[i].identificador;
-			arquivo_csv >> lixo; //lixo nesse caso são as vírgulas
-			
-			arquivo_csv >> lixo;
-			getline(arquivo_csv, satelites[i].nome, '"');
-			arquivo_csv >> lixo;
-			
-			arquivo_csv >> lixo;
-			getline(arquivo_csv, satelites[i].paisOrigem, '"');
-			arquivo_csv >> lixo;
-			
-			arquivo_csv >> satelites[i].anoLancamento;
-			arquivo_csv >> lixo >> lixo;
-			
-			getline(arquivo_csv, satelites[i].funcao, '"');
-			
-			cout << satelites[i].anoLancamento << endl;
-		}
-		
-		delete [] satelites;
-	}
+    //leitura do arquivo csv
+    const string nomeEntrada = "db_satelites.csv";
+    ifstream arquivo_csv(nomeEntrada);
+    
+    if (!arquivo_csv) {
+        cout << "Erro: não foi possível localizar o arquivo \"" << nomeEntrada << "\" :(" << endl;
+    }
 
-	return 0;
+    // Retira linha de comentário
+    string linha;
+    getline(arquivo_csv, linha);
+    
+    int numRegistros;
+    arquivo_csv >> numRegistros;
+    arquivo_csv.ignore();
+    
+    const int TAM_INICIAL = 40;
+    int tamVetor = TAM_INICIAL;
+
+    infoSatelite *satelites = new infoSatelite[TAM_INICIAL];
+    
+    char lixo;
+
+    for (int i = 0; i < numRegistros; i++) {
+        if (i + 1 > tamVetor) {
+            RedimensionaVetor(satelites, tamVetor);
+        }
+
+        arquivo_csv >> satelites[i].identificador;
+        // Lixo, nesse caso são as vírgulas.
+        arquivo_csv >> lixo;
+        
+        arquivo_csv >> lixo;
+        getline(arquivo_csv, satelites[i].nome, '"');
+        arquivo_csv >> lixo;
+        
+        arquivo_csv >> lixo;
+        getline(arquivo_csv, satelites[i].paisOrigem, '"');
+        arquivo_csv >> lixo;
+        
+        arquivo_csv >> satelites[i].anoLancamento;
+        arquivo_csv >> lixo >> lixo;
+        
+        getline(arquivo_csv, satelites[i].funcao, '"');
+    }
+    
+    infoSatelite elemento = {};
+
+    void InserirElemento(infoSatelite elemento, infoSatelite* satelites, int tamVetor);
+
+    delete [] satelites;
+
+    return 0;
+}
+
+void RedimensionaVetor(infoSatelite *&vetor, int &tamanhoVetor) {
+    const int TAM_REDIMENSIONAMENTO = 10;
+    const int TAM_INICIAL = tamanhoVetor;
+
+    infoSatelite *aux = new infoSatelite[TAM_INICIAL];
+    for (int i = 0; i < TAM_INICIAL; i++) {
+        aux[i] = vetor[i];
+    }
+    delete[] vetor;
+
+    tamanhoVetor += TAM_REDIMENSIONAMENTO;
+    vetor = new infoSatelite[tamanhoVetor];
+    for (int i = 0; i < TAM_INICIAL; i++) {
+        vetor[i] = aux[i];
+    }
+    delete[] aux;
+
+    return;
+}
+
+void InserirElemento(infoSatelite elemento, infoSatelite* satelites, int tamVetor) {
+    
+    return; 
 }
